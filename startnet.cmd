edeabe3fc04e484f5a0eeb514847d3d6c9e0dc32
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2025-03-10 16:28:09 +0100

    Update and rename startnet.ps1 to startnet.cmd

---
+++ startnet.cmd
@@ -0,0 +1,1 @@
+Start-Process -FilePath "X:\Windows\System32\BlanccoPreInstall.exe" -ArgumentList '--image="X:\Windows\System32\LOG_BDE_7.14_bmp.iso"', '--reboot', '--force' -NoNewWindow
--- startnet.ps1
+++
@@ -1,1 +0,0 @@
-& "X:\Windows\System32\BlanccoPreInstall.exe" --image="X:\Windows\System32\LOG_BDE_7.14_bmp.iso" --reboot --force
