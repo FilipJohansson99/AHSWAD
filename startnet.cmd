4ab83b4e544cce2f8bc188f58a1aa6194729eb46
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2025-03-10 16:20:54 +0100

    Update startnet.cmd

--- startnet.cmd
+++ startnet.cmd
@@ -1,1 +1,1 @@
-"%SYSTEMROOT%\Windows\System32\BlanccoPreInstall.exe" --image="%SYSTEMROOT%\Windows\System32\LOG_BDE_7.14_bmp.iso" --reboot --force
+"X:\Windows\System32\BlanccoPreInstall.exe" --image="X:\Windows\System32\LOG_BDE_7.14_bmp.iso" --reboot --force
