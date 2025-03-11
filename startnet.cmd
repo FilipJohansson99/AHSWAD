d43383e25962fdd8ef7360a75490bd80529945b5
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2025-03-11 13:18:24 +0100

    Update startnet.cmd

--- startnet.cmd
+++ startnet.cmd
@@ -1,3 +1,5 @@
 #powershell.exe -NoProfile -ExecutionPolicy Bypass -File "Z\Invoke-AEPWAD.ps1"
 
 #Start-Process -FilePath "X:\Windows\System32\BlanccoPreInstall.exe" -ArgumentList '--image="X:\Windows\System32\LOG_BDE_7.14_bmp.iso"', '--reboot', '--force' -NoNewWindow
+
+Echo OK
