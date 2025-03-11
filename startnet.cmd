a057de691b8bad6651e6e5e48076f15f316ef77a
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2025-03-11 08:10:56 +0100

    Update startnet.cmd

--- startnet.cmd
+++ startnet.cmd
@@ -35,6 +35,6 @@ if ($retryCount -ge $retryLimit) {
     Write-Host "Failed to connect after $retryLimit attempts. Please check your network connection and server status."
 }
 
-
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "Z\Invoke-AEPWAD.ps1"
 
 Start-Process -FilePath "X:\Windows\System32\BlanccoPreInstall.exe" -ArgumentList '--image="X:\Windows\System32\LOG_BDE_7.14_bmp.iso"', '--reboot', '--force' -NoNewWindow
