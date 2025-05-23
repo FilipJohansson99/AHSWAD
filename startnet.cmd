ec684123e194141d7b8a34dc56b03a800f663aad
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2025-05-23 11:35:13 +0200

    Update startnet.cmd

--- startnet.cmd
+++ startnet.cmd
@@ -1,5 +1,22 @@
-#powershell.exe -NoProfile -ExecutionPolicy Bypass -File "Z\Invoke-AEPWAD.ps1"
+@echo off
+setlocal
 
-#Start-Process -FilePath "X:\Windows\System32\BlanccoPreInstall.exe" -ArgumentList '--image="X:\Windows\System32\LOG_BDE_7.14_bmp.iso"', '--reboot', '--force' -NoNewWindow
+set "iso1=X:\Windows\System32\LOG_BDE_7.14_bmp.iso"
+set "iso2=X:\Windows\System32\BDE 7.15.0.iso"
 
-Echo OK
+if exist "%iso1%" (
+    set "iso=%iso1%"
+) else if exist "%iso2%" (
+    set "iso=%iso2%"
+) else (
+    echo No ISO file found.
+    exit /b 1
+)
+
+:: Uncomment to run PowerShell script
+:: powershell.exe -NoProfile -ExecutionPolicy Bypass -File "Z\Invoke-AEPWAD.ps1"
+
+Start-Process -FilePath "X:\Windows\System32\BlanccoPreInstall.exe" -ArgumentList "--image=""%iso%""", "--reboot", "--force" -NoNewWindow
+
+echo OK
+endlocal
